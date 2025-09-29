f3c02c63d5d67d0e2544bd63e6e27480b1e1c83b
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-09-29 17:03:24 +0100

    la'implémentation de la classe Incident

--- microfinance-scoringProject/src/model/Incident.java
+++ microfinance-scoringProject/src/model/Incident.java
@@ -1,4 +1,38 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Incident {
+    private LocalDate dateIncident;
+    private Echeance echeance;
+    private int score;
+    private TypeIncident typeIncident;
+
+    public Incident(LocalDate dateIncident, Echeance echeance, int score, TypeIncident typeIncident) {
+        this.dateIncident = dateIncident;
+        this.echeance = echeance;
+        this.score = score;
+        this.typeIncident = typeIncident;
+    }
+
+    // Getters and Setters
+    public LocalDate getDateIncident() { return dateIncident; }
+    public void setDateIncident(LocalDate dateIncident) { this.dateIncident = dateIncident; }
+    public Echeance getEcheance() { return echeance; }
+    public void setEcheance(Echeance echeance) { this.echeance = echeance; }
+    public int getScore() { return score; }
+    public void setScore(int score) { this.score = score; }
+    public TypeIncident getTypeIncident() { return typeIncident; }
+    public void setTypeIncident(TypeIncident typeIncident) { this.typeIncident = typeIncident; }
+
+    @Override
+    public String toString() {
+        return "Incident{" +
+                "dateIncident=" + dateIncident +
+                ", echeance=" + echeance +
+                ", score=" + score +
+                ", typeIncident=" + typeIncident +
+                '}';
+    }
 }
+
