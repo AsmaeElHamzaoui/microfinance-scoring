0ad24fde8d0e50759e612bd58a0872f798091cbb
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-09-29 17:01:09 +0100

    la'implémentation de la classe Professionnel

--- microfinance-scoringProject/src/model/Professionnel.java
+++ microfinance-scoringProject/src/model/Professionnel.java
@@ -1,4 +1,43 @@
 package model;
 
-public class Professionnel {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Professionnel extends Personne {
+    private double revenu;
+    private String immatriculationFiscale;
+    private SecteurActivite secteurActivite;
+    private String activite;
+
+    public Professionnel(String nom, String prenom, LocalDate dateNaissance, String ville, int nombreEnfants,
+                         double investissement, double placement, SituationFamiliale situationFamiliale,
+                         LocalDateTime createdAt, int score, double revenu, String immatriculationFiscale,
+                         SecteurActivite secteurActivite, String activite) {
+        super(nom, prenom, dateNaissance, ville, nombreEnfants, investissement, placement, situationFamiliale, createdAt, score);
+        this.revenu = revenu;
+        this.immatriculationFiscale = immatriculationFiscale;
+        this.secteurActivite = secteurActivite;
+        this.activite = activite;
+    }
+
+    // Getters and Setters
+    public double getRevenu() { return revenu; }
+    public void setRevenu(double revenu) { this.revenu = revenu; }
+    public String getImmatriculationFiscale() { return immatriculationFiscale; }
+    public void setImmatriculationFiscale(String immatriculationFiscale) { this.immatriculationFiscale = immatriculationFiscale; }
+    public SecteurActivite getSecteurActivite() { return secteurActivite; }
+    public void setSecteurActivite(SecteurActivite secteurActivite) { this.secteurActivite = secteurActivite; }
+    public String getActivite() { return activite; }
+    public void setActivite(String activite) { this.activite = activite; }
+
+    @Override
+    public String toString() {
+        return "Professionnel{" + super.toString() +
+                ", revenu=" + revenu +
+                ", immatriculationFiscale='" + immatriculationFiscale + '\'' +
+                ", secteurActivite=" + secteurActivite +
+                ", activite='" + activite + '\'' +
+                '}';
+    }
 }
+
