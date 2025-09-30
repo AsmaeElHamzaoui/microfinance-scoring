fd91cefb127d4473c1b803a248f365bda636a5d9
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-09-30 12:18:56 +0100

    l'implémentation da la fonction ajouterPersonne dans ClientService

--- microfinance-scoringProject/src/service/ClientService.java
+++ microfinance-scoringProject/src/service/ClientService.java
@@ -1,4 +1,19 @@
 package service;
 
+import model.Personne;
+import repository.ClientRepository;
+
+import java.sql.SQLException;
+
 public class ClientService {
+    private static final ClientRepository repoPersone=new ClientRepository();
+
+    public static void ajouterPersonne(Personne p) throws SQLException {
+        try{
+            repoPersone.save(p);
+            System.out.println("le client est ajouté avec succés");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+   }
 }
