bb643af6f6083987935efbc039d53629d4ca2dbd
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-09-30 11:35:16 +0100

    la'implémentation de la classe Database Connection

--- microfinance-scoringProject/src/util/DatabaseConnection.java
+++ microfinance-scoringProject/src/util/DatabaseConnection.java
@@ -1,4 +1,23 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    private static String URL="jdbc:postgresql://localhost:5432/micro_crédit";
+    private static String USERNAME="postgres";
+    private static String PASSWORD="1234";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL,USERNAME,PASSWORD);
+    }
+
+    public static void testConnection() {
+        try (Connection conn = getConnection()) {
+            System.out.println("Connexion à la base de données réussie !");
+        } catch (SQLException e) {
+            System.err.println("Échec de la connexion à la base de données : " + e.getMessage());
+        }
+    }
 }
