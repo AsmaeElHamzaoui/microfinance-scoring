7f1169d2f996b532f5f4d87e9224a9b474e4b709
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-10-02 09:13:56 +0100

    la création du fichier ScoringSystem.java

---
+++ microfinance-scoringProject/src/service/ScoringSystem.java
@@ -0,0 +1,19 @@
+package service;
+
+
+import model.*;
+import model.enums.SituationFamiliale;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Service central pour le calcul du score crédit
+ */
+public class ScoringSystem {
+
+
+}
+
